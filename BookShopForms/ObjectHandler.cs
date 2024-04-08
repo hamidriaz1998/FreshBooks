a74ed0d7a2af3334d9ec17b8e4242d92771f83e7
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-08 06:39:45 +0500

    Add ObjectHandler.cs to handle user and book DLs

---
+++ BookShopForms/ObjectHandler.cs
@@ -0,0 +1,24 @@
+﻿using BookShopForms.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopForms
+{
+    internal class ObjectHandler
+    {
+        private static IUserDL UserDL = UserDB.GetInstance();
+        private static IBookDL BookDL = BookFH.GetInstance();
+        // private static IBookDL BookDL = BookDB.GetInstance();
+        public static IUserDL GetUserDL()
+        {
+            return UserDL;
+        }
+        public static IBookDL GetBookDL()
+        {
+            return BookDL;
+        }
+    }
+}
