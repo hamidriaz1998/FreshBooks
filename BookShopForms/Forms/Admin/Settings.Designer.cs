d65fd58ce4004bec3ea5a2992a6c7edc8a477820
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-08 08:07:12 +0500

    Add Admin Settings Form

---
+++ BookShopForms/Forms/Admin/Settings.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace BookShopForms.Forms.Admin
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.OldPassBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewPassBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConfirmPassBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CurrencyBox = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PassUpdateBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.CurrencyUpdateBtn = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // OldPassBox
+            // 
+            this.OldPassBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.OldPassBox.DefaultText = "";
+            this.OldPassBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.OldPassBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.OldPassBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.OldPassBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.OldPassBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.OldPassBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.OldPassBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.OldPassBox.Location = new System.Drawing.Point(590, 119);
+            this.OldPassBox.Name = "OldPassBox";
+            this.OldPassBox.PasswordChar = '●';
+            this.OldPassBox.PlaceholderText = "";
+            this.OldPassBox.SelectedText = "";
+            this.OldPassBox.Size = new System.Drawing.Size(146, 22);
+            this.OldPassBox.TabIndex = 26;
+            this.OldPassBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label1.Location = new System.Drawing.Point(467, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 20);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Old Password";
+            // 
+            // NewPassBox
+            // 
+            this.NewPassBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NewPassBox.DefaultText = "";
+            this.NewPassBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.NewPassBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.NewPassBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.NewPassBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.NewPassBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.NewPassBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.NewPassBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.NewPassBox.Location = new System.Drawing.Point(590, 172);
+            this.NewPassBox.Name = "NewPassBox";
+            this.NewPassBox.PasswordChar = '●';
+            this.NewPassBox.PlaceholderText = "";
+            this.NewPassBox.SelectedText = "";
+            this.NewPassBox.Size = new System.Drawing.Size(146, 22);
+            this.NewPassBox.TabIndex = 28;
+            this.NewPassBox.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label2.Location = new System.Drawing.Point(460, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "New Password";
+            // 
+            // ConfirmPassBox
+            // 
+            this.ConfirmPassBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ConfirmPassBox.DefaultText = "";
+            this.ConfirmPassBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.ConfirmPassBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.ConfirmPassBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ConfirmPassBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.ConfirmPassBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ConfirmPassBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ConfirmPassBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.ConfirmPassBox.Location = new System.Drawing.Point(590, 231);
+            this.ConfirmPassBox.Name = "ConfirmPassBox";
+            this.ConfirmPassBox.PasswordChar = '●';
+            this.ConfirmPassBox.PlaceholderText = "";
+            this.ConfirmPassBox.SelectedText = "";
+            this.ConfirmPassBox.Size = new System.Drawing.Size(146, 22);
+            this.ConfirmPassBox.TabIndex = 30;
+            this.ConfirmPassBox.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label3.Location = new System.Drawing.Point(447, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Confirm Password";
+            // 
+            // CurrencyBox
+            // 
+            this.CurrencyBox.BackColor = System.Drawing.Color.Transparent;
+            this.CurrencyBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.CurrencyBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CurrencyBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.CurrencyBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.CurrencyBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.CurrencyBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.CurrencyBox.ItemHeight = 30;
+            this.CurrencyBox.Location = new System.Drawing.Point(129, 119);
+            this.CurrencyBox.Name = "CurrencyBox";
+            this.CurrencyBox.Size = new System.Drawing.Size(146, 36);
+            this.CurrencyBox.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label4.Location = new System.Drawing.Point(40, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 40;
+            this.label4.Text = " Currency";
+            // 
+            // PassUpdateBtn
+            // 
+            this.PassUpdateBtn.BorderRadius = 10;
+            this.PassUpdateBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.PassUpdateBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.PassUpdateBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.PassUpdateBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.PassUpdateBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(171)))), ((int)(((byte)(174)))));
+            this.PassUpdateBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassUpdateBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.PassUpdateBtn.Location = new System.Drawing.Point(613, 287);
+            this.PassUpdateBtn.Name = "PassUpdateBtn";
+            this.PassUpdateBtn.Size = new System.Drawing.Size(90, 32);
+            this.PassUpdateBtn.TabIndex = 42;
+            this.PassUpdateBtn.Text = "Update";
+            // 
+            // CurrencyUpdateBtn
+            // 
+            this.CurrencyUpdateBtn.BorderRadius = 10;
+            this.CurrencyUpdateBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.CurrencyUpdateBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.CurrencyUpdateBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.CurrencyUpdateBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.CurrencyUpdateBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(171)))), ((int)(((byte)(174)))));
+            this.CurrencyUpdateBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrencyUpdateBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.CurrencyUpdateBtn.Location = new System.Drawing.Point(161, 172);
+            this.CurrencyUpdateBtn.Name = "CurrencyUpdateBtn";
+            this.CurrencyUpdateBtn.Size = new System.Drawing.Size(90, 32);
+            this.CurrencyUpdateBtn.TabIndex = 43;
+            this.CurrencyUpdateBtn.Text = "Update";
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 490);
+            this.Controls.Add(this.CurrencyUpdateBtn);
+            this.Controls.Add(this.PassUpdateBtn);
+            this.Controls.Add(this.CurrencyBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConfirmPassBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NewPassBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OldPassBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2TextBox ConfirmPassBox;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox NewPassBox;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox OldPassBox;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ComboBox CurrencyBox;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Button CurrencyUpdateBtn;
+        private Guna.UI2.WinForms.Guna2Button PassUpdateBtn;
+    }
+}
