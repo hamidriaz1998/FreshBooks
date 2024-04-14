11723e070758a6c665fe2f2355b6078fb35b8c96
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-14 09:29:18 +0500

    Add OrderHistory form and update BookShopForms.csproj

---
+++ BookShopForms/Forms/Salesmen/OrderHistory.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace BookShopForms.Forms.Salesmen
+{
+    partial class OrderHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CustomerGrid = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OrdersGrid = new Guna.UI2.WinForms.Guna2DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label2.Location = new System.Drawing.Point(324, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 60;
+            this.label2.Text = "Select Customer";
+            // 
+            // CustomerGrid
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.CustomerGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CustomerGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.CustomerGrid.ColumnHeadersHeight = 15;
+            this.CustomerGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CustomerGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            this.CustomerGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.CustomerGrid.Location = new System.Drawing.Point(23, 75);
+            this.CustomerGrid.Name = "CustomerGrid";
+            this.CustomerGrid.RowHeadersVisible = false;
+            this.CustomerGrid.Size = new System.Drawing.Size(745, 124);
+            this.CustomerGrid.TabIndex = 59;
+            this.CustomerGrid.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.CustomerGrid.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.CustomerGrid.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.CustomerGrid.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.CustomerGrid.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.CustomerGrid.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.CustomerGrid.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.CustomerGrid.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.CustomerGrid.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.CustomerGrid.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerGrid.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.CustomerGrid.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.CustomerGrid.ThemeStyle.HeaderStyle.Height = 15;
+            this.CustomerGrid.ThemeStyle.ReadOnly = false;
+            this.CustomerGrid.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.CustomerGrid.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.CustomerGrid.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerGrid.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.CustomerGrid.ThemeStyle.RowsStyle.Height = 22;
+            this.CustomerGrid.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.CustomerGrid.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            this.label1.Location = new System.Drawing.Point(366, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 62;
+            this.label1.Text = "Orders";
+            // 
+            // OrdersGrid
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.OrdersGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(63)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.OrdersGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.OrdersGrid.ColumnHeadersHeight = 15;
+            this.OrdersGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.OrdersGrid.DefaultCellStyle = dataGridViewCellStyle6;
+            this.OrdersGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.OrdersGrid.Location = new System.Drawing.Point(23, 258);
+            this.OrdersGrid.Name = "OrdersGrid";
+            this.OrdersGrid.RowHeadersVisible = false;
+            this.OrdersGrid.Size = new System.Drawing.Size(745, 163);
+            this.OrdersGrid.TabIndex = 61;
+            this.OrdersGrid.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.OrdersGrid.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.OrdersGrid.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.OrdersGrid.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.OrdersGrid.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.OrdersGrid.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.OrdersGrid.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.OrdersGrid.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.OrdersGrid.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.OrdersGrid.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrdersGrid.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.OrdersGrid.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.OrdersGrid.ThemeStyle.HeaderStyle.Height = 15;
+            this.OrdersGrid.ThemeStyle.ReadOnly = false;
+            this.OrdersGrid.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.OrdersGrid.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.OrdersGrid.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrdersGrid.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.OrdersGrid.ThemeStyle.RowsStyle.Height = 22;
+            this.OrdersGrid.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.OrdersGrid.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // OrderHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OrdersGrid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CustomerGrid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "OrderHistory";
+            this.Text = "OrderHistory";
+            this.Load += new System.EventHandler(this.OrderHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2DataGridView CustomerGrid;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2DataGridView OrdersGrid;
+    }
+}
---
+++ BookShopForms/Forms/Salesmen/OrderHistory.cs
@@ -0,0 +1,79 @@
+﻿using Library.DL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Library.BL;
+
+namespace BookShopForms.Forms.Salesmen
+{
+    public partial class OrderHistory : Form
+    {
+        private ICustomerDL CustomerDL = ObjectHandler.GetCustomerDL();
+        private DataTable CustomerDt = new DataTable();
+        private DataTable OrderDt = new DataTable();
+        private int OrderSelectedRow = -1;
+        private int CustomerSelectedRow = -1;
+        public OrderHistory()
+        {
+            InitializeComponent();
+        }
+        private void AddDataRow(Customer c)
+        {
+            DataRow dr = CustomerDt.NewRow();
+            dr["Id"] = c.GetID();
+            dr["Name"] = c.GetName();
+            dr["Email"] = c.GetEmail();
+            dr["Phone"] = c.GetPhone();
+            dr["Address"] = c.GetAddress();
+            CustomerDt.Rows.Add(dr);
+        }
+        private void AddDataRow(Order o)
+        {
+            Book b = o.GetBook();
+            DataRow dr = OrderDt.NewRow();
+            dr["Book name"] = b.GetTitle();
+            dr["Author"] = b.GetAuthor();
+            dr["ISBN"] = b.GetISBN();
+            dr["Price"] = b.GetPrice();
+            dr["Quantity"] = o.GetQuantity();
+            dr["Total"] = o.GetTotal();
+            dr["Date"] = DateTime.Parse(o.GetDate());
+        }
+        private void LoadData()
+        {
+            List<Customer> customers = CustomerDL.GetCustomers();
+            foreach (Customer c in customers)
+            {
+                AddDataRow(c);
+            }
+        }
+
+        private void OrderHistory_Load(object sender, EventArgs e)
+        {   // Configure Customer Grid
+            CustomerDt.Columns.Add("Id", typeof(int));
+            CustomerDt.Columns.Add("Name", typeof(string));
+            CustomerDt.Columns.Add("Email", typeof(string));
+            CustomerDt.Columns.Add("Phone", typeof(string));
+            CustomerDt.Columns.Add("Address", typeof(string));
+            CustomerGrid.ReadOnly = true;
+            CustomerGrid.DataSource = CustomerDt;
+            // Configure Order Grid
+            OrderDt.Columns.Add("Book name", typeof(string));
+            OrderDt.Columns.Add("Author", typeof(string));
+            OrderDt.Columns.Add("ISBN", typeof(string));
+            OrderDt.Columns.Add("Price", typeof(int));
+            OrderDt.Columns.Add("Quantity", typeof(int));
+            OrderDt.Columns.Add("Total", typeof(int));
+            OrderDt.Columns.Add("Date", typeof(DateTime));
+            OrdersGrid.ReadOnly = true;
+            OrdersGrid.DataSource = OrderDt;
+            LoadData();
+        }
+    }
+}
